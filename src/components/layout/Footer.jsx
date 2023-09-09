207cc09b21543b03227ea22f4ae376a2157c1448
pratikash <pratikashpanda.1234@gmail.com>
2023-09-09 23:10:26 +0530

    first commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { AiFillInstagram, AiFillYoutube, AiFillGithub } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>MBA Burger Wala</h2>
+
+        <p>We are trying to give you the best taste possible.</p>
+        <br />
+
+        <em>We give attention to genuine feedback.</em>
+
+        <strong>All right received @mbaburgerwala</strong>
+      </div>
+
+      <aside>
+        <h4>Follow Us</h4>
+
+        <a href="https://youtube.com">
+          <AiFillYoutube />
+        </a>
+        <a href="https://instagram.com">
+          <AiFillInstagram />
+        </a>
+        <a href="https://github.com">
+          <AiFillGithub />
+        </a>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
